b94354a7079faec0dc450a928d31fd7ca0f50970
yotamfried-ux <yotam.fried@gmail.com>
2026-06-23 20:10:22 -0500

    Expose lazy surf editor policy bootstrap

--- pipeline/__init__.py
+++ pipeline/__init__.py
@@ -1,5 +1,8 @@
-"""Pipeline package bootstrap."""
+"""Pipeline package helpers."""
 
-from pipeline.stages import surf_editor as _surf_editor
 
-_surf_editor.install_surf_editor_patches()
+def enable_surf_editor_policy() -> None:
+    """Enable the conservative surf-specific editor policy for pipeline runs."""
+    from pipeline.stages import surf_editor as _surf_editor
+
+    _surf_editor.install_surf_editor_patches()
